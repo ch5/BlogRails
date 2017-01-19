4d76abc3a8d424f3cc1a0a86c5c0fd8a823946bd
ch5 <kholis13@gmail.com>
2017-01-19 09:09:34 +0700

    adding landing page and edit application js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sporckets
+//= require bootstrap-sporockets
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
