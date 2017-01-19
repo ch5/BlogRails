89f9776c3a1b5d87a06be352a3b5950cbd698339
ch5 <kholis13@gmail.com>
2017-01-19 10:48:11 +0700

    adding new active cursor

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/about'
-
-  get 'pages/contact'
-
-  get 'about' => 'pages#about', as: :about
-  get 'contact' => 'pages#contact', as: :contact
+  get 'pages/about', as: :about
+  get 'pages/contact', as: :contact
 
   root to: 'posts#index'
   resources :posts
